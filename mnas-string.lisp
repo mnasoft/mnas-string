f9f8a0a64d8106d235bb194091221f30dc33fc12
Nick Matvyeyev <mnasoft@gmail.com>
2020-01-29 16:36:24 +0200

    Wed Jan 29 14:36:24 UTC 2020

--- mnas-string.lisp
+++ mnas-string.lisp
@@ -4,11 +4,27 @@
 
 (annot:enable-annot-syntax)
 
-@export
 @annot.doc:doc
 "Returns a new string in which all the occurences of the part 
 is replaced with replacement"
 (defun string-replace-all (string part replacement &key (test #'char=))
+  (with-output-to-string (out)
+    (loop with part-length = (length part)
+       for old-pos = 0 then (+ pos part-length)
+       for pos = (search part string
+			 :start2 old-pos
+			 :test test)
+       do (write-string string out
+			:start old-pos
+			:end (or pos (length string)))
+       when pos do (write-string replacement out)
+       while pos)))
+
+@export
+@annot.doc:doc
+"Returns a new string in which all the occurences of the part 
+is replaced with replacement"
+(defun replace-all (string part replacement &key (test #'char=))
   (with-output-to-string (out)
     (loop with part-length = (length part)
        for old-pos = 0 then (+ pos part-length)
